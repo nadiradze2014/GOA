066dae6e6f0d2d94364a58527f80543810ca875a
nadiradze2014 <nikonadiradze2024@gmail.com>
2024-07-23 20:55:11 +0400

    commit

---
+++ Level 03/homework/homework01.py
@@ -0,0 +1,24 @@
+name = 'nikoloz'
+last_name = 'nadiradze'
+age = '9'
+hobby = 'fotball'
+country = 'georgia'
+
+print(name,last_name,age,hobby,country)
+
+
+
+
+print(5+5)
+print(10-5)
+print(345+80)
+print(234+58)
+print(584+10)
+print(834-58)
+print(584-10)
+print(822-100)
+print(345//80)
+print(700//100)
+print(70*10)
+print(5*1)
+print(584**10)
---
+++ Level 04/homewok/homework.py
@@ -0,0 +1,21 @@
+name = "nikoloz"
+last_name = "nadiradze"
+age = 9
+hobby = "football"
+country = "georgia"
+
+print(name,last_name,age,hobby,country)
+
+
+
+
+
+
+
+name = "nikoloz"
+last_name = "nadiradze"
+age = "9"
+hobby = "football"
+country = "georgia"
+
+print(name+last_name+age+hobby+country)
